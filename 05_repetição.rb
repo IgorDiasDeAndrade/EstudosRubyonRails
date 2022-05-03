55d7063508a16e71f14b5b52bad8b7671b6cbc2c
igordiasdeandrade <igor.d.andrade@live.com>
2022-05-03 23:26:32 +0000

    alterado modulo 05

--- 05_repetição.rb
+++ 05_repetição.rb
@@ -1,11 +1,11 @@
-#i = 0
-#num = 5
-
-#while i < num do
-#    puts 'contando...' + i.to_s
-#    i += 1
-#end
-
+i = 0
+num = 5
+#while é um loop que se repete enquanto uma condição for verdadeira
+while i < num do
+    puts 'contando...' + i.to_s
+    i += 1
+end
+#.each é usado para percorrer ranges e arrays
 (1..5).each do |item|
     puts 'o valor lido foi: ' + item.to_s
 end
